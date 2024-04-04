c75fb2b55eeb7aa108213b70c0eff77c8c674936
RushikeshAvhad <avhadrushikesh1804@gmail.com>
2024-04-04 10:45:23 +0530

    Take User Input for Doctor Name and Hospital Name

--- Program.cs
+++ Program.cs
@@ -21,7 +21,7 @@ namespace HospitalAppointment
                 Console.WriteLine("2. Show All Booked Appointment Details");
                 Console.WriteLine("3. Exit");
 
-                Console.WriteLine("Enter your choice: ");
+                Console.Write("Enter your choice: ");
 
                 int choice;
                 if (!int.TryParse(Console.ReadLine(), out choice))
@@ -48,12 +48,47 @@ namespace HospitalAppointment
             }
         }
 
-        private static void ShowAllAppointment()
+        private static void BookAppointment()
+        {
+            Console.WriteLine("\nBook an Appointment");
+
+            //  Accept user input for Hospital Name
+            Console.Write("Enter Hospital Name: ");
+            string hospitalName = Console.ReadLine().ToLower().Replace(" ", "");
+
+            if (IsHospitalNamePresent(hospitalName, "D:\\AvhadRushikesh\\Hospital.csv"))
+            {
+                Console.WriteLine("Hospital Found!");
+            }
+            else
+            {
+                Console.WriteLine("Hospital Not Found.");
+            }
+
+            Console.WriteLine("Enter Doctor Name: ");
+            string doctorName = Console.ReadLine().ToLower().Replace(" ", "");
+
+            if (IsDoctorNamePresent(doctorName, "D:\\AvhadRushikesh\\Doctor.csv"))
+            {
+                Console.WriteLine("Doctor Found!");
+            }
+            else
+            {
+                Console.WriteLine("Doctor Not Found.");
+            }
+        }
+
+        private static bool IsDoctorNamePresent(string doctorName, string v)
         {
             throw new NotImplementedException();
         }
 
-        private static void BookAppointment()
+        private static bool IsHospitalNamePresent(string hospitalName, string v)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void ShowAllAppointment()
         {
             throw new NotImplementedException();
         }
